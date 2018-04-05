5ef7c028a9f6f2f8ba4b325a3ec29f31b7770711
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-04-05 16:25:28 -0600

    Adds Image Search Service
    * Needs refactor, but service working

---
+++ app/services/image_search_service.rb
@@ -0,0 +1,22 @@
+class ImageSearchService
+  def initialize(parkcode = "all")
+    @parkcode = parkcode
+  end
+
+  def self.search
+    raw = new.get_images
+    ImageList.new(raw).images
+  end
+
+  def get_images
+    conn = Faraday.new("https://developer.nps.gov/api/v1/parks")
+    response = conn.get do |req|
+      req.params["api_key"] = ENV["NPS_API_KEY"]
+      req.params["parkcode"] = File.read("./app/data/parkcodes.csv")
+      req.params["fields"] = "images"
+      req.headers["Accept"] = "application/json"
+    end
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
