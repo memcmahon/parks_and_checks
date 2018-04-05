e6c1652d900ddd1450e096d324b925a605c1b39f
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-04-05 12:35:48 -0600

    Adds new routes * Adds favorites route * Adds gifts route * Adds profile route

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   get '/logout', to: 'sessions#destroy'
 
   get '/dashboard', as: 'dashboard', to: 'dashboard#index'
+  get '/profile', as: 'profile', to: 'user#show'
+  get '/gifts', as: 'gifts', to: 'gifts#index'
+  get '/favorites', as: 'favorites', to: 'favorites#index'
 end
