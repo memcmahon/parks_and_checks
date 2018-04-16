6d899c76d5f5083d496e1189649cb12a583b3d64
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-04-16 16:13:02 -0600

    Adds gift creation to charges controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,10 +1,14 @@
 class ChargesController < ApplicationController
   def new
     @description = params[:park]
+    @image_url = params[:url]
   end
 
   def create
     CreateChargeJob.perform_later(500, params[:stripeEmail], params[:stripeToken], params[:description])
+
+    image = Image.find_by(url: params[:url])
+    current_user.gifts.create(image: image, amount: 500)
 
     redirect_to dashboard_path
 
