281969e2d57c45a073ba2bf28ec414fb8b914c7f
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-04-17 13:07:10 -0600

    Adds url check

--- app/models/image_list.rb
+++ app/models/image_list.rb
@@ -9,16 +9,18 @@ class ImageList
   def create_images(info)
     images = info.map do |park|
       park[:images].map do |image|
-        response = Faraday.get(image[:url])
-        if response.status == 200 && response.body.size < 20971520
-          Image.create(park: park[:name],
-                       park_url: park[:url],
-                       credit: image[:credit],
-                       alt_text: image[:altText],
-                       title: image[:title],
-                       caption: image[:caption],
-                       url: Cloudinary::Uploader.upload(image[:url], transformation: [{:width=>500}])["secure_url"],
-                       nps_id: image[:id])
+        if Faraday.get(image[:url])
+          response = Faraday.get(image[:url])
+          if response.status == 200 && response.body.size < 20971520
+            Image.create(park: park[:name],
+                         park_url: park[:url],
+                         credit: image[:credit],
+                         alt_text: image[:altText],
+                         title: image[:title],
+                         caption: image[:caption],
+                         url: Cloudinary::Uploader.upload(image[:url], transformation: [{:width=>500}])["secure_url"],
+                         nps_id: image[:id])
+          end
         end
       end
     end.flatten
