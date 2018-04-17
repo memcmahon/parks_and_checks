8e391ec60ca0673769d6f2afa27ce317af91b1e5
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-04-17 16:19:23 -0600

    updates image create

--- app/models/image_list.rb
+++ app/models/image_list.rb
@@ -9,20 +9,22 @@ class ImageList
   def create_images(info)
     images = info.map do |park|
       park[:images].map do |image|
-        url = begin
-                Cloudinary::Uploader.upload(image[:url], transformation: [{width: 1000, crop: :fit}])["secure_url"]
-              rescue CloudinaryException
-                next
-              end
+        unless Image.find_by(nps_id: image[:id])
+          url = begin
+                  Cloudinary::Uploader.upload(image[:url], transformation: [{width: 1000, crop: :fit}])["secure_url"]
+                rescue CloudinaryException
+                  next
+                end
 
-        Image.find_or_create_by(park: park[:name],
-                     park_url: park[:url],
-                     credit: image[:credit],
-                     alt_text: image[:altText],
-                     title: image[:title],
-                     caption: image[:caption],
-                     url: url,
-                     nps_id: image[:id])
+          Image.create(park: park[:name],
+                       park_url: park[:url],
+                       credit: image[:credit],
+                       alt_text: image[:altText],
+                       title: image[:title],
+                       caption: image[:caption],
+                       url: url,
+                       nps_id: image[:id])
+       end
       end
     end.flatten
   end
