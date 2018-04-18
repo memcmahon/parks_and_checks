d7296ca0b47b87c11d2d9f4f8851344125998612
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-04-18 11:09:34 -0600

    Adds case insensitive search

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
   def index
     if params[:park]
-      @images = Image.where(park: params[:park])
+      @images = Image.where("park ILIKE ?", "%#{params[:park]}%")
     else
       @images = Image.all.sample(20)
     end
