0fa8c2c896f030a664df607652b09f3b4f9c7931
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-04-18 08:58:55 -0600

    Adds Image list spec and job spec

--- spec/jobs/create_charge_job_spec.rb
+++ spec/jobs/create_charge_job_spec.rb
@@ -1,5 +1,19 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe CreateChargeJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe CreateChargeJob, :type => :job do
+  describe "#perform_later" do
+    it "creates a charge in the queue" do
+      ActiveJob::Base.queue_adapter = :test
+      expect {
+        CreateChargeJob.perform_later(500, "test@example.com", "tok_1CIH3yH6rrVqTtkJalCXSCKF", "test")
+      }.to enqueue_job
+    end
+
+    it "creates a charge now" do
+      ActiveJob::Base.queue_adapter = :test
+      expect {
+        CreateChargeJob.perform_now(500, "test@example.com", "tok_1CIH3yH6rrVqTtkJalCXSCKF", "test")
+      }
+    end
+  end
 end
